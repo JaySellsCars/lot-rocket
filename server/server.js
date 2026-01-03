3f5c84610fcf8dc1db8851a7f1c8a040021f24c6
JaySellsCars <jason.pangman@gmail.com>
2026-01-02 20:13:51 -0500

    Update server.js

--- server/server.js
+++ server/server.js
@@ -1,9 +1,11 @@
 // /server/server.js  (REPLACE ENTIRE FILE)
+const fetch = require("node-fetch");
 const express = require("express");
 const path = require("path");
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+
 
 // ===============================
 // BODY PARSING (REQUIRED)
